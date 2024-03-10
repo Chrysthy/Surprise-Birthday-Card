7e208216863a18fd8add771f66e23547338def48
Chrystine Martins <chrystine.martins@outlook.com>
2024-03-10 16:32:00 -0300

    add new content

---
+++ index.js
@@ -0,0 +1,24 @@
+import JSConfetti from "./node_modules/js-confetti";
+
+const jsConfetti = new JSConfetti();
+
+document.getElementById("confetti-button").addEventListener("click", fireConfetti);
+
+function fireConfetti() {
+    jsConfetti.addConfetti({
+        confettiRadius: 1,
+        confettiColors: ['#000', 'hotpink', 'gold'],
+        confettiNumber: 100,
+        emojis: ['💜', '✨', '🎉', '🔥'],
+        emojiSize: 100,
+    });
+}
+
+
+
+
+
+
+
+
+
